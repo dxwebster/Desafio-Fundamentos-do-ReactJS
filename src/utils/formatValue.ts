a6948a7c38c3a5b4a7a03def2199e2bf1c5074f5
Adriana Lima <adrianalimafm@gmail.com>
2020-08-13 21:53:03 -0300

    Upload da Aplicação

---
+++ src/utils/formatValue.ts
@@ -0,0 +1,8 @@
+// Formata os valores na moeda e lingua através da função Intl
+const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+
+export default formatValue;
